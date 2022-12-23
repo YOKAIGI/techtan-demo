2fb4051ea8feabca5d406ad2a55cf43b760a138b
Kaito-Dogi <kendobu0405@gmail.com>
2022-12-24 04:18:15 +0900

    ✨ 信号の例が完成

---
+++ contents/if/light.js
@@ -0,0 +1,32 @@
+class Light {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.radius = 50;
+  }
+
+  draw() {
+    if (this.color == "RED") {
+      fill("#ff0000");
+    } else if (this.color == "BLUE") {
+      fill("#0000ff");
+    }
+
+    ellipse(this.x, this.y, this.radius * 2);
+  }
+
+  onClick() {
+    if (dist(touches[0].x, touches[0].y, this.x, this.y) < this.radius) {
+      lightColor = this.color;
+
+      if (lightColor == "RED") {
+        playerSpeed = 0;
+      } else if (lightColor == "BLUE") {
+        playerSpeed = 2;
+      }
+
+      setCode(lightColor);
+    }
+  }
+}
--- contents/if/sketch.js
+++ contents/if/sketch.js
@@ -1,17 +1,18 @@
 let isPlaying = false;
 let lightColor = "RED";
 let playerSpeed = 0;
+let playerPosition = 0;
 
-const code = `light_color = "${lightColor}"
-player_speed = ${playerSpeed}
+let redLight;
+let blueLight;
 
+function setCode(lightColor) {
+  const code = `light_color = "${lightColor}"
+  
 if light_color == "RED":
   player_speed = 0
 else:
-  player_speed = 10
-`;
-
-function setCode() {
+  player_speed = 2`;
   document.querySelector("code").innerHTML = code;
   hljs.initHighlightingOnLoad();
 }
@@ -29,8 +30,23 @@ function setup() {
   const canvas = createCanvas(windowWidth, windowWidth);
   canvas.parent("canvas");
   setCode();
+
+  redLight = new Light(width / 2, (height * 1) / 4, "RED");
+  blueLight = new Light(width / 2, (height * 3) / 4, "BLUE");
 }
 
 function draw() {
   background("#c9c9c9");
+
+  redLight.draw();
+  blueLight.draw();
+
+  fill("#00ff00");
+  playerPosition = playerPosition + playerSpeed;
+  rect(playerPosition, height - 40, 20, 40);
+}
+
+function touchMoved() {
+  redLight.onClick();
+  blueLight.onClick();
 }
