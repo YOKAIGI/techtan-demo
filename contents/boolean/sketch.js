451badd8940531acdb186e9bd463153ea7562d14
Kaito-Dogi <kendobu0405@gmail.com>
2023-03-13 22:54:11 +0900

    ✨ True, False を表示できるようにした

--- contents/boolean/sketch.js
+++ contents/boolean/sketch.js
@@ -1,25 +1,30 @@
-function setCode() {
-  const code = `# スイッチの変数
-switch = False
-
-# スイッチの切り替え
-switch = not switch`;
-  document.querySelector("code").innerHTML = code;
-  hljs.initHighlightingOnLoad();
-}
-
-function handleReload() {
-  console.log("Reload Button Clicked!");
-}
+let isLighting = false;
 
 function preload() {}
 
 function setup() {
   const canvas = createCanvas(windowWidth, windowWidth);
   canvas.parent("canvas");
-  setCode();
+  setCode(isLighting);
 }
 
 function draw() {
   background("#48B0D2");
 }
+
+const setCode = (isLighting) => {
+  const str = isLighting.toString();
+  const code = `# スイッチの変数
+switch = ${toUpperCamelCase(str)}
+
+# スイッチの切り替え
+switch = not switch`;
+  document.querySelector("code").innerHTML = code;
+  hljs.initHighlightingOnLoad();
+};
+
+const handleReload = () => {
+  console.log("Reload Button Clicked!");
+};
+
+const toUpperCamelCase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
