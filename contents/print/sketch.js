28b716f04b76c05c76079219f6933abd3e7d79fd
Kaito-Dogi <kendobu0405@gmail.com>
2023-03-13 16:07:41 +0900

    💄 print の基本的なレイアウトを作成

--- contents/print/sketch.js
+++ contents/print/sketch.js
@@ -1,3 +1,7 @@
+let parakeetImg;
+let balloon;
+let isSubmitted;
+
 function setCode() {
   const code = `# 好きな言葉を入力する
 message = input('ｺﾝﾆﾁﾊ')
@@ -12,7 +16,10 @@ function handleReload() {
   console.log("Reload Button Clicked!");
 }
 
-function preload() {}
+function preload() {
+  parakeetImg = loadImage("assets/parakeet.png");
+  balloonImg = loadImage("assets/balloon.png");
+}
 
 function setup() {
   const canvas = createCanvas(windowWidth, windowWidth);
@@ -22,4 +29,30 @@ function setup() {
 
 function draw() {
   background("#48B0D2");
+  imageMode(CENTER);
+
+  drawBalloon();
+  drawParakeet();
+}
+
+function drawParakeet() {
+  const parakeetImgWidth = width / 3;
+  image(
+    parakeetImg,
+    width * (3 / 4),
+    height * (3 / 4),
+    parakeetImgWidth,
+    parakeetImg.height * (parakeetImgWidth / parakeetImg.width)
+  );
+}
+
+function drawBalloon() {
+  const balloonImgWidth = width * (2 / 3);
+  image(
+    balloonImg,
+    width * (2 / 5),
+    height * (1 / 3),
+    balloonImgWidth,
+    balloonImg.height * (balloonImgWidth / balloonImg.width)
+  );
 }
