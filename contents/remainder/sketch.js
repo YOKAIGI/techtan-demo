7d37a29c2450cdd3de645264caf18d68897a8168
Kaito-Dogi <kendobu0405@gmail.com>
2023-03-14 04:45:44 +0900

    💄 皿を追加

--- contents/remainder/sketch.js
+++ contents/remainder/sketch.js
@@ -1,31 +1,77 @@
-function setCode() {
-  const code = `# 餃子の数
-gyoza_count = 100
+let isDivided = false;
 
-# 分ける人数を入力
-people_count = input('何人で分ける？')
+let dishImg;
+let largeDishImg;
+let gyozaImg;
 
-# 一人当たりの餃子の計算
-my_gyoza_count = gyoza_count // int(people_count)
-
-# 餃子の余り
-rest_gyoza_count = gyoza_count % int(people_count)`;
-  document.querySelector("code").innerHTML = code;
-  hljs.initHighlightingOnLoad();
+function preload() {
+  dishImg = loadImage("assets/dish.png");
+  largeDishImg = loadImage("assets/large_dish.png");
+  gyozaImg = loadImage("assets/gyoza.png");
 }
-
-function handleReload() {
-  console.log("Reload Button Clicked!");
-}
-
-function preload() {}
 
 function setup() {
   const canvas = createCanvas(windowWidth, windowWidth);
   canvas.parent("canvas");
   setCode();
+  imageMode(CENTER);
 }
 
 function draw() {
   background("#48B0D2");
+
+  drawDishes();
 }
+
+const setCode = () => {
+  const code = `# 餃子の数
+gyoza_count = 7
+
+# 分ける人数
+people_count = 3
+
+# 一人当たりの餃子の計算
+my_gyoza_count = gyoza_count // people_count
+
+# 餃子の余り
+rest_gyoza_count = gyoza_count % people_count`;
+  document.querySelector("code").innerHTML = code;
+  hljs.initHighlightingOnLoad();
+};
+
+const handleReload = () => {
+  isDivided = false;
+};
+
+const drawDishes = () => {
+  image(
+    largeDishImg,
+    width / 2,
+    height / 4,
+    width * 0.9,
+    largeDishImg.height * ((width * 0.9) / largeDishImg.width)
+  );
+
+  const dishImgWidth = width / 3;
+  image(
+    dishImg,
+    width / 6,
+    height * (3 / 4),
+    dishImgWidth,
+    dishImg.height * (dishImgWidth / dishImg.width)
+  );
+  image(
+    dishImg,
+    width / 2,
+    height * (3 / 4),
+    dishImgWidth,
+    dishImg.height * (dishImgWidth / dishImg.width)
+  );
+  image(
+    dishImg,
+    width * (5 / 6),
+    height * (3 / 4),
+    dishImgWidth,
+    dishImg.height * (dishImgWidth / dishImg.width)
+  );
+};
